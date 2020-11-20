6216fbe5c7bcec49389276da86c4cbcf585c18b1
jacobhsu <jacob.hsu.tw@gmail.com>
2020-11-20 11:01:43 +0800

    Login Form svg-sprite-loader

--- src/icons/index.js
+++ src/icons/index.js
@@ -8,5 +8,3 @@ const req = require.context('./svg', false, /\.svg$/)
 
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
-
-console.log('todo: ', requireAll(req))
